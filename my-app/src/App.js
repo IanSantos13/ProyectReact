45b6c7eeb322398720c0dc7bef4f4cd9dd0c952f
IanSa <iansantos113@outlook.com.ar>
2023-10-30 12:38:16 -0300

    Avances para Segunda Preentrega

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 <NavBar />
 
 <ItemListContainer greeting={"Bienvenidos"}/>
+<ItemCount initial={1} stock={10} onAdd={(quantity) => console.log("Cantidad agregada" ,quantity)}/>
     </div>
   );
 }
