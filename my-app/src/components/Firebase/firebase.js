0f38c218cdc6ef959bc4c8ee20b1a75f12aad82e
IanSa <iansantos113@outlook.com.ar>
2023-11-22 22:27:31 -0300

    Firebase maomeno

---
+++ my-app/src/components/Firebase/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA1SG8XO9PA8ttlqa-G2HeOvn6loyQzqzs",
+    authDomain: "coder-commerce-933e6.firebaseapp.com",
+    projectId: "coder-commerce-933e6",
+    storageBucket: "coder-commerce-933e6.appspot.com",
+    messagingSenderId: "187433370340",
+    appId: "1:187433370340:web:1a910932e956a05bff22f1"
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
